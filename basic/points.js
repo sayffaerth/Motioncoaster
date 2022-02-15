8da74a5bdc20c110c7f26afe3530ba40e0729f73
ru89vis <clara.sayffaerth@campus.lmu.de>
2022-02-15 12:53:35 +0100

    Appending points to curve

--- basic/points.js
+++ basic/points.js
@@ -1,4 +1,5 @@
-var groupObject3D = document.querySelector('#track1').object3D;
+//var groupObject3D = document.querySelector('#track1').object3D;
+//console.log(groupObject3D)
 /*groupObject3D.children[2].position.set(100, 20, 30);
 console.log(groupObject3D.children[2])
 
@@ -7,22 +8,37 @@ for (i=0; i < groupObject3D.children.length; i++){
 }*/
 
 var pointlist = [
-    (0, 1, 2),
-    (4, 7, 3),
-    (9, 3, 6)
+    //(0, 1, 2),
+    (4, 7, 3)
+    //(9, 3, 6)
 ]
 
-function defaultTrack(){
-    console.log("Ich funktioniere!")
-    
-}
+var groupObject3D_1 = document.querySelector('#track1');
+var groupObject3D_2 = document.querySelector('#track2');
+var groupObject3D_s = document.querySelector('#blacksphere');
+//for (i=0; i < pointlist.length; i++){
+    var entityEl_1 = document.createElement('a-curve-point')
+    var entityEl_2 = document.createElement('a-curve-point')
+    entityEl_1.setAttribute('position', {x: 2, y: 4, z: 1})
+    entityEl_2.setAttribute('position', {x: 2, y: 4+0.2, z: 1})
+    //entityEl.setAttribute('position', {x: pointlist[i][0], y: pointlist[i][1], z: pointlist[i][2]})
+    groupObject3D_1.appendChild(entityEl_1);
+    groupObject3D_2.appendChild(entityEl_2);
 
-function arTrack(){
-    console.log("Ich funktioniere!")
-    for (i=0; i < groupObject3D.children.length; i++){
-        groupObject3D.children[i].position.set(100, 20, 30);
-    }
-}
+    var endSphere = document.createElement('a-sphere');
+    endSphere.setAttribute('position', {x: 2, y: 4, z: 1});
+    endSphere.setAttribute('radius', 0.5)
+    endSphere.setAttribute('color', "black")
+    groupObject3D_s.appendChild(endSphere);
+//}
+/*
+var groupObject3D = document.querySelector('#track1');
+var entityEl = document.createElement('a-curve-point');
+entityEl.setAttribute('position', {x: -2, y: 1, z: 3});
+console.log(entityEl)
+
+*/
+
 /*
 fs.writeFile('./indexdefault.html', newhtml, (err) => {
     // In case of a error throw err.
